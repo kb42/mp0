5d3c5fc3ba6864801ce891beda026ba3ce72990e
kb42 <kb42@illinois.edu>
2025-09-02 14:12:28 -0500

    Completed MP0

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,24 @@
 /* Your JS here. */
 console.log('Hello World!')
+
+const content = document.querySelector('.content');
+
+function changeColor(color) {
+    const card = document.querySelector('.card');
+    card.style.backgroundColor = color;
+}
+
+document.getElementById('redBtn').addEventListener('click', () => changeColor('rgba(216, 32, 32, 0.821)'));
+document.getElementById('blueBtn').addEventListener('click', () => changeColor('rgba(32, 57, 216, 0.82)'));
+document.getElementById('greenBtn').addEventListener('click', () => changeColor('rgba(10, 157, 57, 0.82)'));
+
+const image = document.getElementById('profile');
+const heading = document.getElementById('name');
+
+image.addEventListener('click', () => {
+    content.style.display = 'none';
+});
+
+heading.addEventListener('click', () => {
+    content.style.display = 'block';
+});
